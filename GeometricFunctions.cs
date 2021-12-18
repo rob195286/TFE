f7e60bd3b10b1b65d2df2ad4c58aa8eb06193569
rob <195286@ecam.be>
2021-12-18 21:08:05 +0100

    ajouter gestion Fonction geometric

---
+++ GeometricFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFE
+{
+    public static class GeometricFunctions
+    {
+        public static double GetDistanceBetweenCoord(double latitude, double longitude)
+        {
+            return 0.0;
+        }
+
+    }
+}
