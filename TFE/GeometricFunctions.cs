6d2853419f528955c282890563a6d58c1901d382
rob <195286@ecam.be>
2021-12-24 10:13:19 +0100

    refactor GeomFunction, integration de la vitesse dans la fonction qui calcul temps

--- TFE/GeometricFunctions.cs
+++ TFE/GeometricFunctions.cs
@@ -4,13 +4,7 @@
 namespace TFE
 {
     public static class GeometricFunctions
-    {
-        /// <summary>
-        ///     1) 120 / 3600 = 1 / 30 -> km / s 
-        ///     2) 100 / 3 -> m / s
-        ///     3) 100 / 3 = 33,33 -> m / s
-        /// </summary>
-        public static double CarSpeed = 33.33; 
+    {        
         /// <summary>
         ///     Permet de calculer la distance entre deux points sur une sphère, prit d'ici : https://fr.wikipedia.org/wiki/Formule_de_haversine
         /// </summary>
@@ -29,12 +23,18 @@ namespace TFE
                                                                               (Math.PI*longitudeSource/180)) /2), 2))));
         }
 
-        public static double TimeAsCrowFlies(GraphNode currentNode, GraphNode targetNode)
+        public static double TimeAsCrowFlies(GraphNode currentNode, GraphNode targetNode, double carSpeed = 120)
         {
+            /// <summary>
+            ///     1) 120 / 3600 = 1 / 30 -> km / s   | speed * 1000 / 3600 -> m / s
+            ///     2) 1 * 1000 / 30       -> m / s    | speed * 10 / 36     -> m / s
+            ///     3) 100 / 3 = 33,33     -> m / s    | speed * 5 / 18      -> m / s
+            /// </summary>
+            carSpeed = carSpeed == 120 ? 33.33 : carSpeed * 5 / 18; 
             return GeometricFunctions.Haversine(currentNode.latitude,
                                                 currentNode.longitude,
                                                 targetNode.latitude,
-                                                targetNode.longitude) / CarSpeed;
+                                                targetNode.longitude) / carSpeed;
         }
 
     }
